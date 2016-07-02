49587215b2a751bad28ad0a2c7df57e24af0dd3f
Robert White <robert@terracoding.com>
2016-07-02 22:07:19 +0100

    Visual Tweaking - Include example PDF content

--- app/pdfs/bill_pdf.rb
+++ app/pdfs/bill_pdf.rb
@@ -1,12 +1,31 @@
 class BillPDF < Prawn::Document
+  include ActionView::Helpers::NumberHelper
+  include BillHelper
+
   def initialize(bill)
     @bill = bill
     super(page_size: 'A4', page_layout: :portrait)
 
-    text 'Latest Bill', size: 32
-    stroke_horizontal_rule
-
+    draw_header
     move_down 30
-    text 'Insert bill details based on expanded HTML views here'
+    draw_subscription_info
+  end
+
+  def draw_header
+    text 'Latest Bill', size: 32
+    fill_color '646464'
+    draw_text "Due #{date_string(@bill[:statement][:due])}", size: 16, at: [145, y-22]
+    fill_color '000000'
+    draw_text currency(@bill[:total]), size: 18, at: [460,y-22]
+    stroke_horizontal_rule
+  end
+
+  def draw_subscription_info
+    text 'Packages', size: 18
+    move_down 10
+    packages = @bill[:package][:subscriptions].map { |s| [s[:name], currency(s[:cost])] }
+    table(packages, cell_style: {borders: [:top, :bottom]}, column_widths: [470, 50])
+    move_down 52
+    draw_text currency(@bill[:package][:total]), at: [475, y], style: :bold
   end
 end
