7d0b2781d3266c18801c969fe32d52a3b713c784
Robert White <robert@terracoding.com>
2016-07-02 13:11:13 +0100

    Add success case to treat handle Bill as a hash

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -4,7 +4,33 @@ class Bill
   def self.latest
     json = API.get(END_POINT)
 
-    return nil if json.blank?
+    return if json.blank?
+
+    Bill.new(json)
+  end
+
+  def initialize(source)
+    @source = underscore_keys(source)
+  end
+
+  def method_missing(*args)
+    @source.send(*args)
+  end
+
+  private
+
+  def underscore_keys(value)
+    case value
+    when Array
+      value.map(&method(:underscore_keys))
+    when Hash
+      Hash[value.map { |k, v| [underscore_key(k), underscore_keys(v)] }]
+    else
+      value
+    end
+  end
+
+  def underscore_key(key)
+    key.to_s.underscore.to_sym
   end
 end
-
--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -2,21 +2,42 @@ require 'rails_helper'
 
 describe Bill do
   describe '.latest' do
+    subject(:bill) { Bill.latest }
+
     context 'when able to pull the bill do' do
+      before do
+        allow(API).to receive(:get).and_return(
+          YAML.load_file(Rails.root.join('spec/fixtures/success.yml'))
+        )
+      end
+
+      it 'returns a Bill object' do
+        expect(bill).to be_a Bill
+      end
+
+      it 'behaves as a hash' do
+        expect(bill[:total]).to eql 100.63
+        expect(bill[:call_charges][:total]).to eql 4.26
+      end
+
+      it 'underscores the keys from the JSON format' do
+        expect(bill.keys).to include *[:sky_store, :call_charges]
+        expect(bill[:sky_store].keys).to include :buy_and_keep
+      end
     end
 
     context 'when unable to pull the bill' do
       context 'when an exception is raised' do
         it 'returns nil' do
           expect(Net::HTTP).to receive(:get).and_raise(RuntimeError.new('Some exception'))
-          expect(Bill.latest).to be_nil
+          expect(bill).to be_nil
         end
       end
 
       context 'when the end-point returns an empty object' do
         it 'returns nil' do
           expect(Net::HTTP).to receive(:get).and_return('{}')
-          expect(Bill.latest).to be_nil
+          expect(bill).to be_nil
         end
       end
     end
