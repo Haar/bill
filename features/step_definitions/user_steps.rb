1c18b40d17154d20f2eeefcade0613b3d9639128
Robert White <robert@terracoding.com>
2016-07-02 13:12:13 +0100

    Add overarching cuke for handling Bill UI/UX

---
+++ features/step_definitions/user_steps.rb
@@ -0,0 +1,27 @@
+Given(/^I am signed up$/) do
+  # N/A - No authentication required
+end
+
+Given(/^I have at least one bill$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I am on the bills page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see my latest bill$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should be able to drill down my services$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should be able to drill down my TV charges$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should be able to drill down my Phone charges$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ features/user_viewing_bills.feature
@@ -0,0 +1,11 @@
+Feature: User - Billing
+  User views billing history
+
+  Scenario: User views their latest bill
+    Given I am signed up
+    And I have at least one bill
+    When I am on the bills page
+    Then I should see my latest bill
+    And I should be able to drill down my services
+    And I should be able to drill down my TV charges
+    And I should be able to drill down my Phone charges
